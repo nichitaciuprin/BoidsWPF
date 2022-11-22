47d496db1e8ba1008bc3a99e782c2262df9e78af
Nichita Ciuprin <nichitaciuprin@gmail.com>
2022-11-22 15:15:05 +0200

    UPDATE

--- Boids_C/src/Subgen.h
+++ Boids_C/src/Subgen.h
@@ -9,16 +9,13 @@
 int state[55];
 int si = 0;
 int sj = 0;
-int i = 0;
-int j = 0;
-int x = 0;
-int p2 = 1;
 
 int Subgen_Next();
 void Subgen_Init(int seed)
 {
 	state[0] = seed % MAX;
-	for (i = 1, j = 21; i < 55; i++, j += 21)
+    int p2 = 1;
+	for (int i = 1, j = 21; i < 55; i++, j += 21)
     {
 		if (j >= 55) j -= 55;
 		state[j] = p2;
@@ -29,14 +26,14 @@ void Subgen_Init(int seed)
 	si = 0;
 	sj = 24;
 
-	for (i = 0; i < 165; i++) Subgen_Next();
+	for (int i = 0; i < 165; i++) Subgen_Next();
 }
 int Subgen_Next()
 {
 	if (si == sj) Subgen_Init(0);
 	if (!si--) si = 54;
 	if (!sj--) sj = 54;
-    x = state[si] - state[sj];
+    int x = state[si] - state[sj];
 	if (x < 0) x += MAX;
 	return state[si] = x;
 }
