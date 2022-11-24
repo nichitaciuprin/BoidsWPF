dc13ccbbea09f3ca92ad7b49f7d5917e1489e977
Nichita Ciuprin <nichitaciuprin@gmail.com>
2022-11-24 17:46:04 +0200

    UPDATE

--- C#/Run/WindowProcess.cs
+++ C#/Run/WindowProcess.cs
@@ -13,6 +13,7 @@ public class WindowProcess : Application
     private Canvas canvas;
     private Queue<MyTransform> myTransformPool;
     private Queue<Shape> boidShapePool;
+    private Queue<Line> linePool;
 
     public static Thread Start(Game game)
     {
@@ -34,14 +35,11 @@ public class WindowProcess : Application
         canvas = CreateCanvas();
         myTransformPool = CreateMyTransformPool(game.boids.Length);
         boidShapePool = CreateBoidShapePool(game.boids.Length);
+        linePool = LinePool(100);
 
-        CreateBorder();
-        // Test_Elipse();
-        // Test_Square();
-        // Test_BoidPolygon();
-        // Test_Cross();
         Render(game);
         window.Show();
+
         window.KeyDown += OnKeyDown;
         Startup += OnStartup;
         Exit += OnExit;
@@ -82,7 +80,7 @@ public class WindowProcess : Application
         window.WindowStyle = WindowStyle.None;
         window.ResizeMode = ResizeMode.NoResize;
         window.AllowsTransparency = false;
-        window.Title = "TestWPF";
+        window.Title = "BOIDS_C#";
         window.Left = 0;
         window.Top = 0;
         var size = new Vector2(600,600);
@@ -111,12 +109,127 @@ public class WindowProcess : Application
             canvas.Children.Add(item);
         return boidShapePool;
     }
+    private Queue<Line> LinePool(int size)
+    {
+        var boidShapePool = new Queue<Line>((new bool[size]).Select(x => new Line()));
+        foreach (var item in boidShapePool)
+            canvas.Children.Add(item);
+        return boidShapePool;
+    }
+
     private void Render(Game gameState)
     {
         HideAll();
         Render(gameState.boids);
+        Render(gameState.aabb);
     }
-    private void CreateBorder()
+    private void Render(AABB aabb)
+    {
+        var p0 = new Vector2(aabb.MinX,aabb.MinY);
+        var p1 = new Vector2(aabb.MinX,aabb.MaxY);
+        var p2 = new Vector2(aabb.MaxX,aabb.MaxY);
+        var p3 = new Vector2(aabb.MaxX,aabb.MinY);
+        RenderLine(p0,p1);
+        RenderLine(p1,p2);
+        RenderLine(p2,p3);
+        RenderLine(p3,p0);
+    }
+    private void Render(Boid[] boids)
+    {
+        if (boids.Length == 0) return;
+        RenderBoid(ref boids[0],Brushes.Red);
+        for (int i = 1; i < boids.Length; i++)
+            RenderBoid(ref boids[i],Brushes.White);
+    }
+    private void RenderBoid(ref Boid boid, Brush color)
+    {
+        var shape = Next(boidShapePool);
+        var myTransform = Next(myTransformPool);
+        shape.Fill = color;
+        shape.Visibility = Visibility.Visible;
+        SetPosition(boid,myTransform,shape);
+    }
+    private void RenderLine(Vector2 start, Vector2 end)
+    {
+        var line = Next(linePool);
+        line.Stroke = Brushes.DarkCyan;
+        // line.Fill = lineBrush;
+        line.StrokeThickness = 1;
+        SetPosition(line,start,end);
+    }
+    private Shape CreateBoidShape()
+    {
+        var shape = new Polygon();
+        var points = new []
+        {
+            new Point(0, 0),
+            new Point(-4, 1),
+            new Point(0, -8),
+            new Point(4, 1),
+            new Point(0, 0),
+        };
+        var duno = 1f/8f/2f*scale;
+        for (int i = 0; i < points.Length; i++)
+            points[i] = new Point(points[i].X*duno,points[i].Y*duno);
+        for (int i = 0; i < points.Length; i++)
+            shape.Points.Add(points[i]);
+        return shape;
+    }
+    private void HideAll()
+    {
+        foreach (var item in boidShapePool) item.Visibility = Visibility.Hidden;
+        // foreach (var item in linePool) item.Visibility = Visibility.Hidden;
+    }
+    private float GetAngle(Vector2 vel)
+    {
+        if (Vector2.IsZero(vel)) return 0;
+        return MathF.Atan2(vel.x,vel.y) * 180f / MathF.PI;
+    }
+    private void SetPosition(Boid boid, MyTransform myTransform, Shape boidShape)
+    {
+        myTransform.Set(boidShape,ToRenderPos(boid.pos),GetAngle(boid.vel));
+    }
+    private void SetPosition(Line line, Vector2 worldPosition_Start, Vector2 worldPosition_End)
+    {
+        worldPosition_Start = Vector2.Mul(worldPosition_Start,scale);
+        worldPosition_End = Vector2.Mul(worldPosition_End,scale);
+        var offsetY = (float)canvas.Height;
+        line.X1 = worldPosition_Start.x;
+        line.Y1 = offsetY - worldPosition_Start.y;
+        line.X2 = worldPosition_End.x;
+        line.Y2 = offsetY - worldPosition_End.y;
+    }
+    private Vector2 ToRenderPos(Vector2 worldPos) => (new Vector2(worldPos.x*scale,(float)canvas.Height - worldPos.y*scale));
+    private class MyTransform
+    {
+        private TransformGroup tg;
+        private TranslateTransform tt;
+        private RotateTransform rt;
+
+        public MyTransform()
+        {
+            tg = new TransformGroup();
+            tt = new TranslateTransform();
+            rt = new RotateTransform();
+            tg.Children.Add(rt);
+            tg.Children.Add(tt);
+        }
+        public void Set(Shape shape, Vector2 pos, float rot)
+        {
+            tt.X = pos.x;
+            tt.Y = pos.y;
+            rt.Angle = rot;
+            shape.RenderTransform = tg;
+        }
+    }
+    private T Next<T>(Queue<T> queue)
+    {
+        var result = queue.Dequeue();
+        queue.Enqueue(result);
+        return result;
+    }
+
+    private void Test_CreateWindowBorder()
     {
         var size = 2;
         var color = Brushes.DarkCyan;
@@ -216,91 +329,5 @@ public class WindowProcess : Application
         var tran = new MyTransform();
         tran.Set(shape,new Vector2((float)canvas.Width/2f,(float)canvas.Height/2),90);
         canvas.Children.Add(shape);
-    }
-    private void Render(AABB aabb)
-    {
-        // var length = 4;
-        // for (int i = 0; i < length; i++)
-        // {
-        // }
-    }
-    private void Render(Boid[] boids)
-    {
-        if (boids.Length == 0) return;
-        RenderBoid(boids,0,Brushes.Red);
-        for (int i = 1; i < boids.Length; i++)
-            RenderBoid(boids,i,Brushes.White);
-    }
-    private void RenderBoid(Boid[] boids, int i, SolidColorBrush color)
-    {
-        var boid = boids[i];
-        var shape = boidShapePool.Dequeue(); boidShapePool.Enqueue(shape);
-        var myTransform = myTransformPool.Dequeue(); myTransformPool.Enqueue(myTransform);
-        shape.Fill = color;
-        shape.Visibility = Visibility.Visible;
-        SetPosition(boid,myTransform,shape);
-    }
-    private Shape CreateBoidShape()
-    {
-        var shape = new Polygon();
-        var points = new []
-        {
-            new Point(0, 0),
-            new Point(-4, 1),
-            new Point(0, -8),
-            new Point(4, 1),
-            new Point(0, 0),
-        };
-        var duno = 1f/8f/2f*scale;
-        for (int i = 0; i < points.Length; i++)
-            points[i] = new Point(points[i].X*duno,points[i].Y*duno);
-        for (int i = 0; i < points.Length; i++)
-            shape.Points.Add(points[i]);
-        return shape;
-    }
-    private void HideAll()
-    {
-        foreach (var item in boidShapePool) item.Visibility = Visibility.Hidden;
-        // foreach (var item in linePool) item.Visibility = Visibility.Hidden;
-    }
-    private void SetPosition(Boid boid, MyTransform myTransform, Shape boidShape)
-    {
-        myTransform.Set(boidShape,ToRenderPos(boid.pos),GetAngle(boid.vel));
-    }
-    private float GetAngle(Vector2 vel)
-    {
-        if (Vector2.IsZero(vel)) return 0;
-        return MathF.Atan2(vel.x,vel.y) * 180f / MathF.PI;
-    }
-    private void SetPosition(Line line, Vector2 worldPosition_Start, Vector2 worldPosition_End)
-    {
-        var offsetY = (float)canvas.Height;
-        line.X1 = worldPosition_Start.x;
-        line.Y1 = offsetY - worldPosition_Start.y;
-        line.X2 = worldPosition_End.x;
-        line.Y2 = offsetY - worldPosition_End.y;
-    }
-    private Vector2 ToRenderPos(Vector2 worldPos) => (new Vector2(worldPos.x*scale,(float)canvas.Height - worldPos.y*scale));
-    private class MyTransform
-    {
-        private TransformGroup tg;
-        private TranslateTransform tt;
-        private RotateTransform rt;
-
-        public MyTransform()
-        {
-            tg = new TransformGroup();
-            tt = new TranslateTransform();
-            rt = new RotateTransform();
-            tg.Children.Add(rt);
-            tg.Children.Add(tt);
-        }
-        public void Set(Shape shape, Vector2 pos, float rot)
-        {
-            tt.X = pos.x;
-            tt.Y = pos.y;
-            rt.Angle = rot;
-            shape.RenderTransform = tg;
-        }
     }
 }
